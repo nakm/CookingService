05a29edc8e8fe01e7756cb485f738a05a3880898
nakm <nak178@gmail.com>
2017-04-13 23:57:47 +0900

    Initial commit

---
+++ src/main/java/org/nakm/cookingservice/CookingServiceApplication.java
@@ -0,0 +1,12 @@
+package org.nakm.cookingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookingServiceApplication.class, args);
+	}
+}
